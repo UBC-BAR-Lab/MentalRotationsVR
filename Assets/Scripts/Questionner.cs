1176462bb794168a7ae9ea8d29789a667a8b0b17
v8t7 <v8t7@ugrad.cs.ubc.ca>
2019-07-26 13:55:02 -0700

    added more prefabs, merged headdata scripts in

---
+++ Assets/Scripts/Questionner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questionner : MonoBehaviour
+{
+	public int currentQuestionNum;
+	void Start(){currentQuestionNum=0;}
+
+	public int nextQuestion(){
+		currentQuestionNum++;
+		return currentQuestionNum;
+	}
+}
